a20473317118e5152bb019bbfb656026a2110d36
lamekino <zoey.j.mcbride@gmail.com>
2023-06-14 18:59:49 -0400

    use c9x

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -x
-gcc -std=c89 -ggdb -pedantic -Wall -Wextra -Werror -D_XOPEN_SOURCE=500 -DDEBUG \
+gcc -std=c9x -ggdb -pedantic -Wall -Wextra -Werror -D_XOPEN_SOURCE=500 -DDEBUG \
     -lpthread -o lookpath lookpath.c
