947cc293ec44a69f1993154b3177a228b272aaa2
lamekino <zoey.j.mcbride@gmail.com>
2023-07-23 11:59:55 -0400

    generalize listing flag descriptions

--- src/usage.c
+++ src/usage.c
@@ -4,20 +4,23 @@
 #include "usage.h"
 #include "arguments.h"
 
+static void list_flags(const char *title,
+                       enum arguments start,
+                       enum arguments end) {
+    enum arguments it = start;
+    printf("  %s:\n", title);
+    while (++it < end) {
+        printf("    %s:  %s\n", get_argument(it), get_description(it));
+    }
+
+}
+
 void usage(const char *prog_name) {
     enum arguments it;
 
     printf("%s:\n", prog_name);
 
-    printf("  display options:\n");
-    it = START_PRINT_FLAGS;
-    while (++it < END_PRINT_FLAGS) {
-        printf("    %s:  %s\n", get_argument(it), get_description(it));
-    }
-
-    printf("  miscellaneous:\n");
-    it = START_MISC_FLAGS;
-    while (++it < END_MISC_FLAGS) {
-        printf("    %s:  %s\n", get_argument(it), get_description(it));
-    }
+    list_flags("display options", START_PRINT_FLAGS, END_PRINT_FLAGS);
+    list_flags("query options", START_QUERY_FLAGS, END_QUERY_FLAGS);
+    list_flags("miscellaneous", START_MISC_FLAGS, END_MISC_FLAGS);
 }
