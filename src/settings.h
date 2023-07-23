dbdcec47a7e78b4cd32123cbfac780c255dd11b0
lamekino <zoey.j.mcbride@gmail.com>
2023-07-23 12:08:17 -0400

    create enum order, WIP: drop dependancy on xmacro here

--- src/settings.h
+++ src/settings.h
@@ -1,12 +1,28 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
+#include <stddef.h>
 #include <stdbool.h>
 
 #include "arguments.h"
 
+enum order {
+    IN_PLACE,
+    REVERSE,
+    LEN_ORDER
+};
+
+enum print_mode {
+#define PRINT_MODE_ENUM(label, ...) PM_##label,
+    PRINT_FLAGS(PRINT_MODE_ENUM)
+#undef PRINT_MODE_ENUM
+    LEN_PRINT_MODES
+};
+
 typedef struct {
     enum print_mode print_mode;
+    enum order order;
     char *pattern;
+    size_t pattern_len;
 } settings_t;
 
 #endif
