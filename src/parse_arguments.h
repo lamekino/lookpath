7aa7ade4ff5da72008b4c288001d1f4a6ea892f6
lamekino <zoey.j.mcbride@gmail.com>
2023-07-24 02:02:43 -0400

    remove junk

--- src/parse_arguments.h
+++ src/parse_arguments.h
@@ -1,18 +1,9 @@
 #ifndef PARSE_ARGUMENTS_H
 #define PARSE_ARGUMENTS_H
+
 #include "arguments.h"
+#include "errors.h"
 #include "settings.h"
 
-#if 0
-const char *flag_descs[] = {
-    [SORTED] = "prints filenames as a sorted list (default)",
-    [FULL_PATH] = "prints full paths, unsorted as given by readdir(2)",
-    [FULL_PATH_SORTED] = "prints full path sorted by basename",
-    [TREE] = "prints matches as a tree, unsorted, as given by readdir(2)",
-    [TREE_SORTED] = "prints as a tree with sorted elements",
-    [TREE_SORTED + 1] = "print this help"
-};
-#endif
-
-int parse_arguments(settings_t *settings, int argc, char **argv);
+enum error parse_arguments(settings_t *settings, int argc, char **argv);
 #endif
