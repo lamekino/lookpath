d0f7f7ee35db03e31737de94a9312479443e7cc8
lamekino <zoey.j.mcbride@gmail.com>
2023-07-24 23:14:03 -0400

    generate enum search_methods from xmarco

--- src/search_methods.h
+++ src/search_methods.h
@@ -1,14 +1,18 @@
 #ifndef SEARCH_METHODS_H
 #define SEARCH_METHODS_H
 
+#include "debug_assert.h"
+#include "arguments.h"
+
 #include <stddef.h>
 #include <stdbool.h>
 
 typedef bool (*search_method_fp)(const char *, const char *, size_t);
 
 enum search_methods {
-    LEFT_TO_RIGHT,
-    RIGHT_TO_LEFT,
+#define SEARCH_METHOD_ENUM(label, ...) SM_##label,
+    QUERY_FLAGS(SEARCH_METHOD_ENUM)
+#undef SEARCH_METHOD_ENUM
 
     NUM_SEARCH_METHODS
 };
